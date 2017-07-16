7c0667fbd84ba6138ca05246a604da30f9585e2f
atul <atulmishra1996@gmail.com>
2017-07-16 19:59:12 +0530

    Adding Setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='CodechefScrapper',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Atul Mishra',
+    author_email='atulmishra1996@gmail.com',
+    description='A simple Graphical based application for Codechef Rankings'
+)
