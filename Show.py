1404ea54768c500704495131505509bd25f20b0c
atul <atulmishra1996@gmail.com>
2017-07-16 15:50:17 +0530

    added Gui

---
+++ Show.py
@@ -0,0 +1,38 @@
+import sys
+from PyQt5.QtWidgets import (QWidget,QApplication,QVBoxLayout,QPushButton,QLineEdit,QLabel,QHBoxLayout)
+import main
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.label = QLabel("Enter Handle ")
+        self.name_input = QLineEdit()
+        self.button = QPushButton("Search")
+        self.button.clicked.connect(self.clickedButton)
+        h = QHBoxLayout()
+        h.addWidget(self.label)
+        h.addWidget(self.name_input)
+
+        v = QVBoxLayout()
+        v.addStretch(1)
+        v.addLayout(h)
+        v.addWidget(self.button)
+        self.setLayout(v)
+        self.setWindowTitle("Codechef Scrapper")
+        self.show()
+
+    def clickedButton(self):
+        print("bv")
+        print(main.cnt)
+        self.button.setText("your Rank is "+str(main.res))
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window=MainWindow()
+    #print(main.cnt)
+    sys.exit(app.exec_())
